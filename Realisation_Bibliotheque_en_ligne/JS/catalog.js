5d7be2defce5653bf2924667ad40933635d3ceb0
soulayman-7x <idbahasevenx@gmail.com>
2025-11-13 15:21:02 +0000

    Add the Free books

--- Realisation_Bibliotheque_en_ligne/JS/catalog.js
+++ Realisation_Bibliotheque_en_ligne/JS/catalog.js
@@ -3,12 +3,12 @@ let library = JSON.parse(localStorage.getItem('library')) || [
   { code: 0, title: "STORY OF THE 7X", author: "SOULAYMAN 7X", year: 2019, available: true, price: 550, image: "BOOKS/7X-BOOK (1).png" },
   { code: 1, title: "Dune", author: "Frank Herbert", year: 1965, available: true, price: 300, image: "BOOKS/Dune.png" },
   { code: 2, title: "Great Expectations", author: "Charles Dickens", year: 1861, available: true, price: 280, image: "BOOKS/Great-Expectations.png" },
-  { code: 3, title: "Jane Eyre", author: "Charlotte Brontë", year: 1847, available: true, price: 270, image: "BOOKS/Jane-Eyre.png" },
+  { code: 3, title: "Jane Eyre", author: "Charlotte Brontë", year: 1847, available: true, price: 0, image: "BOOKS/Jane-Eyre.png" },
   { code: 4, title: "Meditations", author: "Marcus Aurelius", year: 180, available: true, price: 250, image: "BOOKS/Meditations.png" },
-  { code: 5, title: "Moby Dick", author: "Herman Melville", year: 1851, available: true, price: 290, image: "BOOKS/Moby-Dick.png" },
+  { code: 5, title: "Moby Dick", author: "Herman Melville", year: 1851, available: true, price: 0, image: "BOOKS/Moby-Dick.png" },
   { code: 6, title: "Pride and Prejudice", author: "Jane Austen", year: 1813, available: true, price: 260, image: "BOOKS/Pride-and-Prejudice.png" },
   { code: 7, title: "The 7 Habits", author: "Stephen R. Covey", year: 1989, available: true, price: 340, image: "BOOKS/The-7-Habits.png" },
-  { code: 8, title: "The Art of War", author: "Sun Tzu", year: -500, available: true, price: 220, image: "BOOKS/The-Art-of-War.png" },
+  { code: 8, title: "The Art of War", author: "Sun Tzu", year: -500, available: true, price: 0, image: "BOOKS/The-Art-of-War.png" },
   { code: 9, title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, available: true, price: 400, image: "BOOKS/The-Lord-of-the-Rings.png" },
   { code: 10, title: "Think and Grow Rich", author: "Napoleon Hill", year: 1937, available: true, price: 350, image: "BOOKS/Think-and-Grow-Rich.png" },
   { code: 11, title: "Wuthering Heights", author: "Emily Brontë", year: 1847, available: true, price: 275, image: "BOOKS/Wuthering-Heights.png" },
@@ -23,6 +23,10 @@ function showBooks(filterBooks = library) {
   filterBooks.forEach(book => {
     let card = document.createElement('div');
     card.className = 'card';
+
+    if (book.price === 0) {
+      card.className = 'free card'
+    }
 
     if (book.price > 300) {
       card.className = 'expensive card';
@@ -96,13 +100,14 @@ function reserveBook(code) {
 function updateState(expensiveCount) {
   let total = library.length;
   let available = library.filter(book => book.available).length;
+  let freeBooks = library.filter(book => book.price === 0).length;
   document.getElementById('stats').innerHTML = `
     <p>Total Books : <span> ${total}</span></p>
     <p>Expensive Books : <span>${expensiveCount}</span></p>
     <p>Available Books : <span>${available}</span></p>
+    <p>Free Books : <span>${freeBooks}</span></p>
   `;
-}
-
+}  
 
 //Search input function 
 document.getElementById('search-input').addEventListener('input', function () {
